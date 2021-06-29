44d3a017c959e59f4d40a327200c2eee7a840d6a
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-06-29 14:21:26 -0400

    Added Character Controller & ListItem with View

---
+++ SuperHeroDB.Models/CharacterListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Models
+{
+    public class CharacterListItem
+    {
+        [Display(Name = "Character ID")]
+        public int CharacterId { get; set; }
+
+        [Display(Name = "Character Name")]
+        public string Character { get; set; }
+    }
+}
---
+++ SuperHeroDB.WebMVC/Controllers/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using SuperHeroDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroDB.WebMVC.Controllers
+{
+    [Authorize]
+    public class CharacterController : Controller
+    {
+        // GET: Character
+        public ActionResult Index()
+        {
+            var model = new CharacterListItem[0];
+            return View(model);
+        }
+    }
+}
