2b1f0a4193781396e9ec361c860eeace6e6d85fd
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-06-29 10:20:25 -0400

    Add project files.

---
+++ SuperHeroDB.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperHeroDB.WebMVC.MvcApplication" Language="C#" %>
---
+++ SuperHeroDB.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperHeroDB.WebMVC.Startup))]
+namespace SuperHeroDB.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
