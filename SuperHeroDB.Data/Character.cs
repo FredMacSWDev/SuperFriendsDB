f1748b5691b477fb881cc6df40c9ad4f7dd12f91
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-06-29 13:58:33 -0400

    Created Character class; set connection string &I account registration

---
+++ SuperHeroDB.Data/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Data
+{
+    public class Character
+    {
+        [Key]
+        public int CharacterId { get; set; }
+
+        [Required]
+        [Display(Name = "Character")]        
+        public string HeroName { get; set; }
+    }
+}
