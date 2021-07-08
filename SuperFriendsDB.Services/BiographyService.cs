18b06b1d16b1f60e1869393f2bb8d9449adbb496
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-07-08 16:52:39 -0400

    Create Biography Service with  Create & Get methods

--- SuperFriendsDB.Services/BiographyService.cs
+++ SuperFriendsDB.Services/BiographyService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SuperFriendsDB.Data;
+using SuperFriendsDB.Models.BiographyModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,59 @@ namespace SuperFriendsDB.Services
 {
     public class BiographyService
     {
+        private readonly Guid _userId;
+        public BiographyService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateBio(BiographyCreate model)
+        {
+            var entity =
+                new Biography()
+                {
+                    BioId = model.BioId,
+                    CharacterId = model.CharacterId,
+                    FullName = model.FullName,
+                    AlterEgos = model.AlterEgos,
+                    Aliases = model.Aliases,
+                    PlaceOfBirth = model.PlaceOfBirth,
+                    FirstAppearance = model.FirstAppearance,
+                    Publisher = model.Publisher,
+                    Alignment = model.Alignment
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Bio.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+
+        public IEnumerable<BiographyListItem> GetBio()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Bio
+                        .Select(
+                            e =>
+                                new BiographyListItem
+                                {
+                                    BioId = e.BioId,
+                                    CharacterId = e.CharacterId,
+                                    FullName = e.FullName,
+                                    AlterEgos = e.AlterEgos,
+                                    PlaceOfBirth = e.PlaceOfBirth,
+                                    FirstAppearance = e.FirstAppearance,
+                                    Publisher = e.Publisher,
+                                    Alignment = e.Alignment
+                                });
+
+                return query.ToArray();
+            }
+        }
     }
 }
