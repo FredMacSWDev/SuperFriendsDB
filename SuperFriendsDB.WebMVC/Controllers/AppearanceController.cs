fc4fb791d801c2b0ae88f71552d48730eb66a2c1
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-07-09 00:05:53 -0400

    Created Appearance Controller, List Item, & Index View

---
+++ SuperFriendsDB.WebMVC/Controllers/AppearanceController.cs
@@ -0,0 +1,20 @@
+﻿using SuperFriendsDB.Models.AppearanceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperFriendsDB.WebMVC.Controllers
+{
+    [Authorize]
+    public class AppearanceController : Controller
+    {
+        // GET: Appearance
+        public ActionResult Index()
+        {
+            var model = new AppearanceListItem[0];
+            return View();
+        }
+    }
+}
