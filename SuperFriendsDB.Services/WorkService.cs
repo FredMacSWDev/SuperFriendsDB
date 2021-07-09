f67182d201c13730349f9d5369ee1d20e6daf16d
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-07-09 12:13:49 -0400

    Added Work service & create method

---
+++ SuperFriendsDB.Services/WorkService.cs
@@ -0,0 +1,62 @@
+﻿using SuperFriendsDB.Data;
+using SuperFriendsDB.Models.WorkModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFriendsDB.Services
+{
+    public class WorkService
+    {
+        private readonly Guid _userId;
+
+        public WorkService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateWork(WorkCreate model)
+        {
+            var entity =
+                new Work()
+                {
+                    WorkId = model.WorkId,
+                    CharacterId = model.CharacterId,
+                    Occupation = model.Occupation,
+                    Base = model.Base
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.WorkDetails.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+
+        public IEnumerable<WorkListItem> GetWork()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .WorkDetails
+                        .Select(
+                            e =>
+                                new WorkListItem
+                                {
+                                    WorkId = e.WorkId,
+                                    CharacterId = e.CharacterId,
+                                    Occupation = e.Occupation,
+                                    Base = e.Base
+                                });
+
+                return query.ToArray();
+            }
+
+        }
+
+    }
+}
