dc0ec5b2b541832b04be915af650b9f852e8d129
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-07-12 01:57:56 -0400

    Created Connections Data layer, controller, & Index view

---
+++ SuperFriendsDB.WebMVC/Controllers/ConnectionController.cs
@@ -0,0 +1,20 @@
+﻿using SuperFriendsDB.Models.ConnectionModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperFriendsDB.WebMVC.Controllers
+{
+    [Authorize]
+    public class ConnectionController : Controller
+    {
+        // GET: Connection
+        public ActionResult Index()
+        {
+            var model = new ConnectionListItem[0];
+            return View(model);
+        }
+    }
+}
